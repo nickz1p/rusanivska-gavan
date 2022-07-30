e4e108b967ee1ec2d7e218ccb9d1862181b1af7f
Nikolay Popadiuk <nikolay.popadiuk@sigma.software>
2022-07-31 02:28:06 +0300

    test

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-import asyncio
-from pyrogram import Client
-
-api_id = 9963453
-
-api_hash = "3cff28aa68fe29ef514cd004bf50c1ea"
-
-
-async def main():
-    async with Client("my_account", api_id, api_hash) as app:
-        await app.send_message("me", "Greetings from **Pyrogram**!")
-
-
-asyncio.run(main())
+import telebot
+# Создаем экземпляр бота
+bot = telebot.TeleBot('5325079329:AAGqWJjwUr-3ywZ3mkmowAzs8cBNidjqvUA')
+# Функция, обрабатывающая команду /start
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь )')
+# Получение сообщений от юзера
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)
