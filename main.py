e5c839a3ddc93df0fd24235cc035456467bc5daa
Nikolay Popadiuk <nikolay.popadiuk@sigma.software>
2022-07-31 01:48:14 +0300

    test

--- main.py
+++ main.py
@@ -1,48 +1,14 @@
-import logging
-from aiogram import types
-from aiogram.utils.executor import start_webhook
-from config import bot, dp, WEBHOOK_URL, WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT
-from db import database
+import asyncio
+from pyrogram import Client
+
+api_id = 9963453
+
+api_hash = "3cff28aa68fe29ef514cd004bf50c1ea"
 
 
-async def on_startup(dispatcher):
-    await database.connect()
-    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+async def main():
+    async with Client("my_account", api_id, api_hash) as app:
+        await app.send_message("me", "Greetings from **Pyrogram**!")
 
 
-async def on_shutdown(dispatcher):
-    await database.disconnect()
-    await bot.delete_webhook()
-
-
-async def save(user_id, text):
-    await database.execute(f"INSERT INTO messages(telegram_id, text) "
-                           f"VALUES (:telegram_id, :text)", values={'telegram_id': user_id, 'text': text})
-
-
-async def read(user_id):
-    results = await database.fetch_all('SELECT text '
-                                       'FROM messages '
-                                       'WHERE telegram_id = :telegram_id ',
-                                       values={'telegram_id': user_id})
-    return [next(result.values()) for result in results]
-
-
-@dp.message_handler()
-async def echo(message: types.Message):
-  # await save(message.from_user.id, message.text)
-   # messages = await read(message.from_user.id)
-    await message.answer(messages)
-
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        skip_updates=True,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT,
-    )
+asyncio.run(main())
